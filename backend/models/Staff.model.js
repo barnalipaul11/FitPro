6a7b25d307c551c4721dbaf454a7a3afba499aed
barnali <barnalipaul890@gmail.com>
2025-06-20 14:09:06 +0530

    mongo db connect

---
+++ backend/models/Staff.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const staffSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["Personal Trainer", "Admin", "Manager", "Receptionist"],
+    required: true,
+  },
+  shift: {
+    start: { type: String, required: true },
+    end: { type: String, required: true },
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Active", "Inactive"],
+    default: "Active",
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Staff", staffSchema);
